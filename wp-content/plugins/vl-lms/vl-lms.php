96e26f3f908dc13920156b742a3ddcc6afcaa0ad
Tymofii Synianskyi <janosense@gmail.com>
2026-04-22 10:32:45 +0300

    Add initial LMS plugin scaffolding with metadata and configuration files

---
+++ wp-content/plugins/vl-lms/vl-lms.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     LMS
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     vl-lms
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Vl_Lms
+ */
+
+// Your code starts here.
