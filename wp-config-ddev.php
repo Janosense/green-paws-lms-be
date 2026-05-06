2139c1c4252d2621c8a6caa6c68123c1dc711c47
Tymofii Synianskyi <janosense@gmail.com>
2026-05-06 17:27:16 +0300

    update(localhost): change default frontend and LMS app URLs to port 3000 in wp-config-ddev

--- wp-config-ddev.php
+++ wp-config-ddev.php
@@ -32,8 +32,8 @@ if ( getenv( 'IS_DDEV_PROJECT' ) == 'true' ) {
 	defined( 'VL_JWT_AUTH_SECRET_KEY' ) || define( 'VL_JWT_AUTH_SECRET_KEY', 'Pd>2+.^nzKx##/t}gt5yjA=QGPn5|Z*WmSf2vpit&[k{}9x8nAf7Nn~rqZa.5CIb' );
 
 	define('VL_CORS_ORIGINS', 'http://localhost:3000,http://localhost:3001,https://app.vetlms.com,http://localhost:49894');
-	define('VL_FRONTEND_URL', 'http://localhost:3001');
-	define('VL_LMS_APP_URL', 'http://localhost:3001');
+	define('VL_FRONTEND_URL', 'http://localhost:3000');
+	define('VL_LMS_APP_URL', 'http://localhost:3000');
 
 	/**
 	 * Set WordPress Database Table prefix if not already set.
